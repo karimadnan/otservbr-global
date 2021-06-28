dc4c920410e0918cfc28478ce491030fa8979e2e
karimadnan <karimadnan@>
2021-06-28 08:16:00 +0200

    MissingMonster

---
+++ data/monster/quests/dangerous_depth/lava_hole.lua
@@ -0,0 +1,84 @@
+local mType = Game.createMonsterType("Lava Hole")
+local monster = {}
+
+monster.description = "a lava hole"
+monster.experience = 0
+monster.outfit = {
+	lookTypeEx = 388
+}
+
+monster.health = 5000
+monster.maxHealth = 5000
+monster.race = "undead"
+monster.corpse = 0
+monster.speed = 0
+monster.manaCost = 0
+monster.maxSummons = 0
+
+monster.changeTarget = {
+	interval = 4000,
+	chance = 1
+}
+
+monster.strategiesTarget = {
+	nearest = 70,
+	health = 10,
+	damage = 10,
+	random = 10,
+}
+
+monster.flags = {
+	summonable = false,
+	attackable = false,
+	hostile = false,
+	convinceable = false,
+	pushable = false,
+	rewardBoss = false,
+	illusionable = false,
+	canPushItems = false,
+	canPushCreatures = false,
+	staticAttackChance = 95,
+	targetDistance = 1,
+	runHealth = 0,
+	healthHidden = true,
+	isBlockable = false,
+	canWalkOnEnergy = false,
+	canWalkOnFire = true,
+	canWalkOnPoison = false,
+	pet = false
+}
+
+monster.light = {
+	level = 0,
+	color = 0
+}
+
+monster.loot = {
+}
+
+monster.defenses = {
+	defense = 30,
+	armor = 30
+}
+
+monster.elements = {
+	{type = COMBAT_PHYSICALDAMAGE, percent = 100},
+	{type = COMBAT_ENERGYDAMAGE, percent = 100},
+	{type = COMBAT_EARTHDAMAGE, percent = 100},
+	{type = COMBAT_FIREDAMAGE, percent = 100},
+	{type = COMBAT_LIFEDRAIN, percent = 100},
+	{type = COMBAT_MANADRAIN, percent = 100},
+	{type = COMBAT_DROWNDAMAGE, percent = 100},
+	{type = COMBAT_ICEDAMAGE, percent = 100},
+	{type = COMBAT_HOLYDAMAGE , percent = 100},
+	{type = COMBAT_DEATHDAMAGE , percent = 100}
+}
+
+monster.immunities = {
+	{type = "paralyze", condition = false},
+	{type = "outfit", condition = false},
+	{type = "invisible", condition = false},
+	{type = "bleed", condition = false}
+}
+
+mType:register(monster)
